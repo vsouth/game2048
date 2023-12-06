bbbc7815df5713173a48571bb72da467543fc291
vsouth <neppchela@gmail.com>
2023-12-06 23:34:03 +0300

    Implemented moveAndMergeEqual

--- src/main/java/ru/vsouth/GameHelper.java
+++ src/main/java/ru/vsouth/GameHelper.java
@@ -4,6 +4,26 @@ import java.util.List;
 
 public class GameHelper {
     public List<Integer> moveAndMergeEqual(List<Integer> list) {
-        return null;
+        var firstPointerIndex = 0;
+        var secondPointerIndex = 1;
+        while (secondPointerIndex < list.size()) {
+            var firstPointerValue = list.get(firstPointerIndex);
+            var secondPointerValue = list.get(secondPointerIndex);
+            if (firstPointerIndex == secondPointerIndex) {
+                secondPointerIndex++;
+            } else if (secondPointerValue == null) {
+                secondPointerIndex++;
+            } else if (firstPointerValue == null) {
+                list.set(firstPointerIndex, secondPointerValue);
+                list.set(secondPointerIndex, null);
+            } else if (firstPointerValue.equals(secondPointerValue)) {
+                list.set(firstPointerIndex, firstPointerValue * 2);
+                list.set(secondPointerIndex, null);
+                firstPointerIndex++;
+            } else {
+                firstPointerIndex++;
+            }
+        }
+    return list;
     }
 }
